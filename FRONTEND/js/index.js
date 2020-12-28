dc26d1c345c2bc9bb6dc7cdff34aa32948e954ba
EL FAKIH Karim <elfakihkarim@hotmail.Fr>
2020-12-28 18:46:03 +0100

    affichage des produit Accueil

--- FRONTEND/js/index.js
+++ FRONTEND/js/index.js
@@ -17,3 +17,18 @@ fetch("http://localhost:3000/api/cameras")
     console.log(error);
   });
 
+//Notre template camera card
+const cameraCard = () => {
+  $lesCameras.innerHTML += `<div id="camera-card" class="card" style="width: 20em;">
+      <a href="produit.html?id=${camera._id}">
+      <img src="${camera.imageUrl}" class="card-img-top" alt="image de la caméra ${camera.name}">
+      </a>
+      <div class="card-body">
+          <h4 class="card-title">${camera.name}</h4>
+          <p class="card-text">${camera.description}</p> 
+          <a href="produit.html?id=${camera._id}">
+              <button type="button" id="camera-infos" class="add-to-cart  btn btn-dark btn-md">Plus d'informations</button>
+          </a>
+      </div>   
+  </div>`;
+};
