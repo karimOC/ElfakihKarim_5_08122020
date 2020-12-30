9ba6178f6b3130e0ff65a56c76f249ffcc1813ec
EL FAKIH Karim <elfakihkarim@hotmail.Fr>
2020-12-30 13:18:59 +0100

    FIN page produit

--- FRONTEND/js/produit.js
+++ FRONTEND/js/produit.js
@@ -1,46 +1,45 @@
 //Paramètre URL
-const queryString = window.location.search;
-console.log(queryString);
+const queryString = window.location.search; //recupere ladresse entiere
 const urlParams = new URLSearchParams(queryString);
-console.log(queryString);
+const product = urlParams.get("id"); //recupere l'id
+console.log(product);
 
 //Appel de l'API
 async function fillProducts() {
-  await fetch("http://localhost:3000/api/cameras/") // Renverra des informations, mais dans un format incorrect
+  await fetch("http://localhost:3000/api/cameras/" + product) // Renverra des informations, mais dans un format incorrect
     .then((response) => response.json()) //Renvoi la reponse en Json
-    .then((cameras) => idCameras(cameras)) //Appel fonction
+    .then((camera) => idCamera(camera)) //Appel fonction
     .catch((error) => console.log(error));
 }
 fillProducts();
 
 //Fonction pour afficher la liste de toutes les caméras disponible
-function idCameras(array) {
+function idCamera(object) {
+  console.log(object);
   let idChoisie = document.querySelector("#choixCameras");
-  for (let elem of array) {
-    let lenses = elem.lenses;
-    idChoisie.innerHTML += `<div id="camera-card" class="card" style="width: 18em;">
-    <a href="produit.html?id=${elem._id}">
-    <img src="${elem.imageUrl}" class="card-img-top" alt="image de la caméra ${elem.name}">
+  idChoisie.innerHTML += `<div id="camera-card" class="card" style="width: 18em;">
+    <a href="produit.html?id=${object._id}">
+    <img src="${
+      object.imageUrl
+    }" class="card-img-top" alt="image de la caméra ${object.name}">
     </a>
     <div class="card-body">
-        <h4 class="card-title">${elem.name}</h4>
-        <p class="card-text">${elem.description}</p>
-        <select class="custom-select col-sm-4 col-12" id="inputGroupSelect01">
-            ${lenses.innerHTML}
+        <h4 class="card-title">${object.name}</h4>
+        <p class="card-text">${object.description}</p>
+        <select class="custom-select col-sm-4 col-12" id="listLenses">
         </select>
-          <label class="camera-quantity-selector col-sm-4 col-12 text-center " for="camera-quantity">Quantité: 
-            <select id="quantity" onclick="addToPrice()" class="text-center mx-auto" name="camera-quantity">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-          </label>
-        <a href="produit.html?id=${elem._id}">
-        <button type="button"  onclick="addToBasket()" id="camera-buy" class="add-to-products btn btn-warning">Ajouter au panier</button>
+        <strong><p class="card-text mt-3">Prix: ${
+          object.price / 100
+        } €</p></strong>
+        <a href="produit.html?id=${object._id}">
+        <button type="button"  onclick="addToBasket()" id="camera-buy" class="add-to-products btn btn-warning mt-3">Ajouter au panier</button>
         </a>
-
-
     </div>
 </div>`;
+  for (elem of object.lenses) {
+    let emplacementSelect = document.getElementById("listLenses");
+    let option = document.createElement("option");
+    option.text = elem;
+    emplacementSelect.add(option);
   }
 }
