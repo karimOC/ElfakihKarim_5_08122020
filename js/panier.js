6ba20c76db341b7150684b7b805d7cfa2b56a3c0
EL FAKIH Karim <elfakihkarim@hotmail.Fr>
2021-01-12 21:12:00 +0100

    prix quantité panier

--- js/panier.js
+++ js/panier.js
@@ -1,32 +1,11 @@
 let storage = JSON.parse(window.localStorage.getItem("panier"));
-console.log(storage);
 for (let elem of storage) {
   createImage(elem.image);
+  createName(elem.nom, elem.lense);
+  quantityCamera();
+  addOption();
+  priceCamera(elem.prix);
   createButton();
-  // Nom de la caméra et lense
-  let nom = document.createElement("p");
-  let nomEmplacement = document.getElementById("ligne-panier");
-  nom.textContent = elem.nom;
-  let lense = document.createElement("p");
-  let lenseEmplacement = document.getElementById("ligne-panier");
-  lense.textContent = elem.lense;
-  nomEmplacement.appendChild(nom);
-  lenseEmplacement.appendChild(lense);
-  // Quantité
-  let select = document.createElement("select");
-  let selectEmplacement = document.getElementById("ligne-panier");
-  select.className = "quantity";
-  selectEmplacement.appendChild(select);
-
-  // Prix de la caméra
-  let prix = document.createElement("p");
-  let prixEmplacement = document.getElementById("ligne-panier");
-  prixEmplacement.appendChild(prix);
-  // Changement du prix en fonction de la quantité
-  let listQuantities = document.getElementsByClassName("quantity");
-  for (let elem of listQuantities) {
-    elem.addEventListener("change", addToPrice);
-  }
 }
 
 // Image
@@ -37,6 +16,7 @@ function createImage(src) {
   img.className = "w-100";
   imgEmplacement.appendChild(img);
 }
+
 // Boutton supprimer
 function createButton() {
   let btn = document.createElement("button");
@@ -46,31 +26,55 @@ function createButton() {
   btn.textContent = "x";
   btnEmplacement.appendChild(btn);
 }
+
 // Nom de la caméra et lense
-function createButton() {
-  let btn = document.createElement("button");
-  let btnEmplacement = document.getElementById("ligne-panier");
-  btn.type = "button";
-  btn.className = "btn-xs btn-danger mt-1 mb-3";
-  btn.textContent = "x";
-  btnEmplacement.appendChild(btn);
+function createName(name, lense) {
+  let nom = document.createElement("p");
+  let nomEmplacement = document.getElementById("ligne-panier");
+  nom.textContent = name;
+  let lentille = document.createElement("p");
+  let lenseEmplacement = document.getElementById("ligne-panier");
+  lentille.textContent = lense;
+  nomEmplacement.appendChild(nom);
+  lenseEmplacement.appendChild(lentille);
 }
 
-function addToPrice(prix) {
-  var qty = document.getElementsByClassName("quantity");
-  for (let elem of qty) {
-    prix.textContent = qty.value * elem.prix + " €";
+// Creation du <select> pour la quantité
+function quantityCamera() {
+  let select = document.createElement("select");
+  let selectEmplacement = document.getElementById("ligne-panier");
+  select.className = "quantity";
+  selectEmplacement.appendChild(select);
+}
+
+// Ajouter les <option> au <select>
+function addOption() {
+  let optionEmplacement = document.getElementsByClassName("quantity");
+  for (let i = 1; i < 4; i++) {
+    let option = document.createElement("option");
+    option.text = i;
+    option.value = i;
+    for (let elem of optionEmplacement) {
+      elem.add(option);
+    }
   }
 }
 
-let optionEmplacement = document.getElementsByClassName("quantity");
-for (i = 1; i < 4; i++) {
-  let option = document.createElement("option");
-  option.text = i;
-  option.value = i;
-  for (let elem of optionEmplacement) {
-    elem.add(option);
+// Prix de la caméra
+function priceCamera(price) {
+  let prix = document.createElement("p");
+  let prixEmplacement = document.getElementById("ligne-panier");
+  prix.textContent = "Prix: " + price + " €";
+  prix.className = "prix-camera-panier";
+  prixEmplacement.appendChild(prix);
+  // Changement du prix en fonction de la quantité
+  let qtyChoose = document.getElementsByClassName("quantity");
+  for (let elem of qtyChoose) {
+    elem.addEventListener("change", function addToPrice() {
+      console.log(elem.value);
+    });
   }
 }
-addToPrice(prix);
+
+
 // localStorage.clear();
